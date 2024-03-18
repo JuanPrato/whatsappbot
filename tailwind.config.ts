b9dbfc5a9f2b2310fd184d3674217456bdaf696e
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-18 20:06:36 -0300

    colors & animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,15 +13,27 @@ const config: Config = {
         "primary-dark": "#27AE60",
         accent: "#E67E22",
         "accent-dark": "#D35400",
+        text: "#202C38",
+        "text-neg": "#ECF0F1",
+        light: "#ECF0F1",
       },
       keyframes: {
         wiggle: {
           "0%, 100%": { transform: "rotate(-3deg)" },
           "50%": { transform: "rotate(3deg)" },
         },
+        outline: {
+          "0%, 100%": {
+            "outline-color": "#D35400",
+          },
+          "50%": {
+            "outline-color": "#E67E22",
+          },
+        },
       },
       animation: {
         wiggle: "wiggle .5s ease-in-out infinite",
+        outline: "outline .5s ease-in-out infinite",
       },
     },
   },
