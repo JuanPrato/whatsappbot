850c71228d65a8c728aa95e6b8825663985ff798
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-20 20:18:44 -0300

    change login styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,14 +7,14 @@ import {
 export default function LoginPage() {
   return (
     <section className="flex h-[calc(100vh-7rem)] w-full items-center bg-text px-[15%]">
-      <div className="flex w-full flex-col items-start justify-center gap-5">
-        <h2 className="text-3xl font-medium text-text-neg">
+      <div className="flex w-full flex-col items-start justify-center gap-5 rounded-lg bg-light p-5">
+        <h2 className="text-3xl font-medium text-text">
           Ingresa para gestionar tu bot
         </h2>
         <form className="flex w-full flex-col gap-5">
-          <EmailInput label="Email" dark />
-          <PasswordInput label="Contraseña" dark />
-          <ButtonInput label="Ingresar" type="submit" dark />
+          <EmailInput label="Email" />
+          <PasswordInput label="Contraseña" />
+          <ButtonInput label="Ingresar" type="submit" />
         </form>
       </div>
       <div className="flex w-full justify-center overflow-hidden">
