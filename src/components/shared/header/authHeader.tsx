b0dc77b4ac7ec9ecba6b3a43bf9036d86d49e40f
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-22 17:48:07 -0300

    styles header user

--- src/components/shared/header/authHeader.tsx
+++ src/components/shared/header/authHeader.tsx
@@ -1,7 +1,23 @@
+import { RiRobot2Fill } from "react-icons/ri";
+import { PiTableFill } from "react-icons/pi";
+import { RxAvatar } from "react-icons/rx";
+import { FaImages } from "react-icons/fa";
+import { HeaderIcon } from "./HeaderIcon";
+
 export default function AuthHeader() {
   return (
-    <header>
-      <h1>HEADER</h1>
+    <header className="shadow-standard flex h-screen w-28 flex-col items-center bg-light p-3">
+      <div className="w-24 border-b border-text border-opacity-30 pb-3">
+        <img src="/LOGO.png" alt="logo" />
+      </div>
+      <div className="flex h-full w-full flex-col items-center justify-between py-3">
+        <div className="flex flex-col gap-4">
+          <HeaderIcon Icon={RiRobot2Fill} isActive={true} />
+          <HeaderIcon Icon={PiTableFill} />
+          <HeaderIcon Icon={FaImages} />
+        </div>
+        <HeaderIcon Icon={RxAvatar} />
+      </div>
     </header>
   );
 }
