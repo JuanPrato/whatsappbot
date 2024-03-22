0a1e31ca748966bb276b6eadf186733d062b5744
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-22 18:18:09 -0300

    add shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,6 +38,9 @@ const config: Config = {
       boxShadow: {
         standard: `2px 4px 4px 0px rgba(0,0,0,0.25)`,
       },
+      dropShadow: {
+        button: "0px 0px 8px",
+      },
     },
   },
   plugins: [],
