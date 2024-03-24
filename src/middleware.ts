53c5ea5ab0ee8b166148b2e87b96a76cb3fda8f1
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-24 10:31:29 -0300

    add middleware auth check

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./auth/lib";
 
+const UNPROTECTED_ROUTES = ["/", "/login", "register"];
+
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const res = await updateSession(request);
+  const isUnProtectedRoute = UNPROTECTED_ROUTES.some((route) =>
+    request.nextUrl.pathname.startsWith(route),
+  );
+
+  if (isUnProtectedRoute) return res;
+
+  if (!res) return Response.redirect(new URL("/login", request.url));
 }
