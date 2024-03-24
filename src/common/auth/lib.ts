64bc385c330e6dfdb46cfcb982b363b3a4aed3d6
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-24 12:00:49 -0300

    put more time un cookie refresh

--- src/common/auth/lib.ts
+++ src/common/auth/lib.ts
@@ -50,7 +50,7 @@ export async function updateSession(request: NextRequest) {
 
   // Refresh the session so it doesn't expire
   const parsed = await decrypt(session);
-  parsed.expires = new Date(Date.now() + 10 * 1000);
+  parsed.expires = new Date(Date.now() + 60 * 60 * 1000);
   const res = NextResponse.next();
   res.cookies.set({
     name: "session",
