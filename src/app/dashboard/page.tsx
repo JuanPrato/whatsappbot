eb419b2776b9bdce1079698ef0d96f7403be79e8
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-24 13:31:44 -0300

    use userHeader component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,23 @@
-import { ButtonInput } from "@/components/shared/input";
 import { PowerButton } from "@/components/shared/powerButton";
 import UserStatistics from "@/components/dashboard/userStatistics";
 import WelcomeMessage from "@/components/dashboard/welcomeMessage";
+import UserMenu from "@/components/dashboard/menu";
+import { UserCategoryHeader } from "@/components/shared/userCategoryHeader";
 
 export default function DashBoardPage() {
   return (
     <div className="ml-[7rem] flex h-full min-h-screen flex-col gap-6 bg-light px-20 py-12">
-      <header className="flex w-full items-center justify-between">
-        <h1 className="text-5xl">Información y gestión de tu bot</h1>
-        <PowerButton />
-      </header>
+      <UserCategoryHeader
+        title="Información y gestión de tu bot"
+        icon={PowerButton}
+      />
       <section>
         <div className="grid grid-cols-2 gap-16">
           <WelcomeMessage />
           <UserStatistics />
         </div>
       </section>
-      <section>
-        <header className="flex w-full items-center justify-between">
-          <h2 className="text-2xl font-medium">Menú</h2>
-          <ButtonInput label="Importar por excel" />
-        </header>
-      </section>
+      <UserMenu />
     </div>
   );
 }
