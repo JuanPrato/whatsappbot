19c80a749be56c3327d455aef6d5bbb8ab089d5b
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-03-26 20:06:06 -0300

    modal component

---
+++ src/components/shared/modal.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { ReactNode, useRef, HTMLProps, useEffect } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface Props {
+  children?: ReactNode;
+  darkBg?: boolean;
+  open: boolean;
+  onCancel: () => void;
+}
+
+export default function Modal({ children, open, onCancel }: Props) {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (open) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  }, [open]);
+
+  const onClick: HTMLProps<HTMLDialogElement>["onClick"] = (e) => {
+    if (e.target === ref.current) {
+      ref.current.close();
+    }
+  };
+  console.log(open);
+  return (
+    <dialog
+      ref={ref}
+      className={twMerge(
+        "grid place-content-center bg-[rgba(0,0,0,.3)]",
+        !open && "hidden",
+      )}
+      onClick={onClick}
+      onClose={onCancel}
+    >
+      {children}
+    </dialog>
+  );
+}
