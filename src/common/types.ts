c68f5a7ccffe1d356d2d79ce197b979698233a63
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-01 17:10:30 -0300

    add twilio message types

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,3 +3,38 @@ export type MenuItem = {
   title: string;
   reply: string;
 };
+
+export interface IncomingMessage {
+  SmsMessageSid: string;
+  NumMedia: string;
+  ProfileName: string;
+  MessageType: string;
+  SmsSid: string;
+  WaId: string;
+  SmsStatus: string;
+  Body: string;
+  To: string;
+  NumSegments: string;
+  ReferralNumMedia: string;
+  MessageSid: string;
+  AccountSid: string;
+  From: string;
+  ApiVersion: string;
+}
+
+export interface Message {
+  id: number;
+  from: string;
+  message: string | null;
+  timestamp: Date;
+}
+
+export interface Command {
+  title: string;
+  reply: string;
+  images: Image[];
+}
+
+export interface Image {
+  url: string;
+}
