bb6a0205569acfdca693b56e241022f17b83b87e
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-01 18:46:13 -0300

    use user data

--- src/components/dashboard/welcomeMessage.tsx
+++ src/components/dashboard/welcomeMessage.tsx
@@ -1,12 +1,17 @@
 import { TextInput, ButtonInput } from "../shared/input";
 import { Box } from "./box";
 
-export default function WelcomeMessage() {
+interface Props {
+  welcomeMessage: string | null;
+}
+
+export default function WelcomeMessage(props: Props) {
   return (
     <Box title="Define el mensaje de bienvenida">
       <TextInput
         placeholder="Bienvenido a la tienda virtual"
         inputClassName="bg-light"
+        value={props.welcomeMessage || ""}
       />
       <ButtonInput
         label="Guardar"
--- src/components/shared/userCategoryHeader.tsx
+++ src/components/shared/userCategoryHeader.tsx
@@ -3,12 +3,18 @@ import { IconType } from "react-icons";
 interface Props {
   title: string;
   icon?: IconType;
+  phone?: string;
 }
 
 export function UserCategoryHeader(props: Props) {
   return (
     <header className="flex w-full items-center justify-between">
-      <h1 className="text-5xl">{props.title}</h1>
+      <h1 className="text-5xl">
+        {props.title}
+        <span className="ml-2 text-3xl text-text text-opacity-90">
+          {props.phone}
+        </span>
+      </h1>
       {props.icon && <props.icon />}
     </header>
   );
