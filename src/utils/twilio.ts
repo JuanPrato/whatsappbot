b328e9761d51740e2c2372fe86596af9dc3b4e3e
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-01 18:07:04 -0300

    use formData

--- src/utils/twilio.ts
+++ src/utils/twilio.ts
@@ -1,12 +1,10 @@
-import { Request } from "express";
-import { IncomingMessage } from "types";
+import { IncomingMessage } from "@/common/types";
 
-export function extractMessageInformationFromRequest(
-  req: Request<{}, {}, IncomingMessage>
-) {
+export async function extractMessageInformationFromRequest(req: Request) {
+  const formData = await req.formData();
   return {
-    userPhone: req.body.To.slice(9),
-    senderPhone: req.body.From.slice(9),
-    commandTitle: req.body.Body,
+    userPhone: formData.get("To")?.slice(9) as string,
+    senderPhone: formData.get("From")?.slice(9) as string,
+    commandTitle: formData.get("Body") as string,
   };
 }
