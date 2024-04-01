106fdb37ebfdb11c615334ea1e2beb456a1e80c3
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-01 17:33:01 -0300

    configure sqlite file url

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
 const client = createClient({
-  url: "file:db/database.db",
+  url: `file:src/db/database.db`,
 });
 
 export const db = drizzle(client, { schema });
