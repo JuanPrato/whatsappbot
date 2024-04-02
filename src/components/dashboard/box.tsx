ad824b11df692d8f5764956532f206061d080b90
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-01 21:34:10 -0300

    add as prop

--- src/components/dashboard/box.tsx
+++ src/components/dashboard/box.tsx
@@ -1,19 +1,34 @@
-import { ReactNode } from "react";
+import { ComponentProps, ElementType, ReactNode } from "react";
+
+interface PropsWithAs<T extends ElementType> {
+  title: string;
+  children?: ReactNode;
+  as: T;
+  properties: ComponentProps<T>;
+}
 
 interface Props {
   title: string;
   children?: ReactNode;
+  as?: never;
+  properties?: never;
 }
 
-export function Box({ title, children }: Props) {
+export function Box<T extends ElementType>({
+  title,
+  children,
+  as,
+  properties,
+}: Props | PropsWithAs<T>) {
+  const As = as || "div";
   return (
-    <div className="flex flex-col gap-5">
+    <As className="flex flex-col gap-5" {...(properties || {})}>
       <header>
         <h3 className="text-2xl font-medium">{title}</h3>
       </header>
       <div className="flex h-full flex-col gap-3 rounded-lg bg-text p-5">
         {children}
       </div>
-    </div>
+    </As>
   );
 }
