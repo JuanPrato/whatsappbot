32d48e7aa4c4a38c6e84a22b4d2ff0237bdbde93
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-02 22:38:31 -0300

    compatibility to new item

--- src/components/dashboard/editMenuItemModal.tsx
+++ src/components/dashboard/editMenuItemModal.tsx
@@ -11,12 +11,15 @@ import Modal from "../shared/modal";
 import { Command } from "@/common/types";
 import { useFormState, useFormStatus } from "react-dom";
 import { updateMenuItem } from "@/app/dashboard/actions";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
-  item: Command;
+  item?: Command;
+  label?: string;
+  openButtonColor?: string;
 }
 
-export function EditMenuItemModal({ item }: Props) {
+export function EditMenuItemModal({ item, label, openButtonColor }: Props) {
   const [open, setOpen] = useState(false);
   const formRef = useRef<HTMLFormElement>(null);
   const [state, formAction] = useFormState(updateMenuItem, null);
@@ -30,8 +33,8 @@ export function EditMenuItemModal({ item }: Props) {
   return (
     <>
       <ButtonInput
-        label="Editar"
-        inputClassName="min-w-[200px] bg-yellow-500"
+        label={label || "Editar"}
+        inputClassName={twMerge("min-w-[200px] bg-yellow-500", openButtonColor)}
         onClick={() => setOpen(true)}
       />
       <Modal
@@ -43,14 +46,14 @@ export function EditMenuItemModal({ item }: Props) {
       >
         <form
           action={formAction}
-          className="min-w-[350px] rounded-lg bg-light p-5"
+          className="min-w-[350px] overflow-hidden rounded-lg bg-light p-5"
           ref={formRef}
         >
           <TextInput
             placeholder="Title"
             //boxClassName="border-b border-dark py-1"
             inputClassName="border-t-0 border-x-0 rounded-none outline-none border-b borde-dark"
-            value={item.title}
+            value={item?.title}
             error={state?.errors?.title}
             name="title"
             errorClassName="text-dark"
@@ -60,12 +63,12 @@ export function EditMenuItemModal({ item }: Props) {
             label="Respuesta"
             inputClassName="min-h-[150px]"
             boxClassName="border-dark border-b border-opacity-30"
-            value={item.reply}
+            value={item?.reply}
             error={state?.errors?.reply}
             name="reply"
             errorClassName="text-dark"
           />
-          <HiddenInput value={item.id} name="id" />
+          <HiddenInput value={item?.id} name="id" />
           <ButtonInput
             label="Guardar"
             type="submit"
