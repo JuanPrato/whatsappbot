f1184b02e6d7c235704fb25f06ef9c04d4e75f48
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-04 21:34:38 -0300

    styles on image

--- src/components/images/card.tsx
+++ src/components/images/card.tsx
@@ -9,13 +9,13 @@ export function Card({ picture }: Props) {
   return (
     <li
       key={picture.id}
-      className="relative h-auto w-[200px] overflow-hidden rounded-lg"
+      className="relative aspect-square h-auto w-[200px] overflow-hidden rounded-lg"
     >
       <FaTrash className="absolute right-2 top-2 z-10 size-[25px] text-danger transition-transform hover:scale-105" />
       <img
         src={picture.url}
         alt={`Imagen subida`}
-        className="mask-image-linear aspect-square"
+        className="mask-image-linear mx-auto max-h-full max-w-full"
       />
       <h3 className="absolute bottom-2 left-2 text-light">
         {picture.description}
