a4fe784cce9672c0faf2000c704b2a6e58d45351
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-04 21:34:29 -0300

    remove action

--- src/app/images/actions.ts
+++ src/app/images/actions.ts
@@ -6,23 +6,8 @@ import { UTApi } from "uploadthing/server";
 
 const utapi = new UTApi();
 
-export async function uploadImage(formData: FormData) {
-  console.log("PASE");
+export async function deleteImage(formData: FormData) {
   try {
-    const file = formData.get("file") as File;
-    const phone = formData.get("phone") as string;
-    const response = await utapi.uploadFiles([file]);
-
-    if (response[0].data === null) {
-      return {
-        success: false,
-        error: "Error desconocido",
-      };
-    }
-
-    const bot = await Bot.getBot(phone);
-
-    await bot?.addImage(response[0].data.url, file.name);
   } catch (e) {
     console.log(e);
     return {
