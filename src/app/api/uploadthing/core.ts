cd30d8faf7baa461af0eb92870016be485c9d0e2
Juan Manuel Prato <pratojuanmanuel2@gmail.com>
2024-04-06 13:51:55 -0300

    add pdf

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -12,7 +12,10 @@ const auth = () => {
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({
+    image: { maxFileSize: "4MB" },
+    pdf: { maxFileSize: "4MB" },
+  })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
